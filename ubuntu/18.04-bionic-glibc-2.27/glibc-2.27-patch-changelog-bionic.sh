c0e8f9b14f5821c400db14f7c85a8d7542e9aef1
William Poetra Yoga <william.poetra@gmail.com>
2022-10-03 01:23:32 +0700

    fix: create tempfiles only as needed

--- ubuntu/18.04-bionic-glibc-2.27/glibc-2.27-patch-changelog-bionic.sh
+++ ubuntu/18.04-bionic-glibc-2.27/glibc-2.27-patch-changelog-bionic.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-TEMP1=$(mktemp)
-TEMP2=$(mktemp)
-
 # glibc (2.27-3ubuntu1.6) bionic; urgency=medium
 head -n1 glibc-2.27/debian/changelog | grep -wq bionic
 if [ $? -ne 0 ]; then
@@ -10,8 +7,13 @@ if [ $? -ne 0 ]; then
   exit 0
 fi
 
+echo Patching glibc-2.27/debian/changelog
+
+TEMP1=$(mktemp)
+TEMP2=$(mktemp)
+
 cat >$TEMP1 <<EOF
-glibc (999:2.27-3ubuntu1.6bionic1) UNRELEASED; urgency=medium
+glibc (2.27-openvz6+bionic1+3ubuntu1.6) UNRELEASED; urgency=medium
 
   * Build on Bionic on OpenVZ 6
 
