e8584c499d077f6811ca229251bdebcb7f2fed2e
William Poetra Yoga <william.poetra@gmail.com>
2022-10-03 20:14:29 +0700

    fix: build glibc 2.31 on focal

--- ubuntu/20.04-focal-glibc-2.31/glibc-2.31-patch-changelog-focal.sh
+++ ubuntu/20.04-focal-glibc-2.31/glibc-2.31-patch-changelog-focal.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-# glibc (2.31-0ubuntu9.9) focal; urgency=medium
-head -n1 glibc-2.31/debian/changelog | grep -wq focal
+# glibc (2.35-0ubuntu3.1) jammy; urgency=medium
+head -n1 glibc-2.35/debian/changelog | grep -wq jammy
 if [ $? -ne 0 ]; then
   echo Changelog already patched.
   exit 0
 fi
 
-echo Patching glibc-2.31/debian/changelog
+echo Patching glibc-2.35/debian/changelog
 
 TEMP1=$(mktemp)
 TEMP2=$(mktemp)
 
 cat >$TEMP1 <<EOF
-glibc (2.31-openvz6+ubuntu20.04+0ubuntu9.9) UNRELEASED; urgency=medium
+glibc (2.35-openvz6+ubuntu20.04+0ubuntu3.1) UNRELEASED; urgency=medium
 
-  * Build on Focal on OpenVZ 6
+  * Build on Jammy on OpenVZ 6
 
  -- William Poetra Yoga <william.poetra@gmail.com>  Mon, 3 Oct 2022 21:51:37 +0700
 
 EOF
 
-cat $TEMP1 glibc-2.31/debian/changelog >$TEMP2
-cat $TEMP2 >glibc-2.31/debian/changelog
+cat $TEMP1 glibc-2.35/debian/changelog >$TEMP2
+cat $TEMP2 >glibc-2.35/debian/changelog
 rm $TEMP1 $TEMP2
 
